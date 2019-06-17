1a38180bae5105461b3c2f88b7c22688b4e3d8d3
Declan Sullivan <declan@declansullivan.com>
2019-06-17 03:54:06 -0700

    Gets previous tweet to determine next tweet

--- diver.py
+++ diver.py
@@ -7,29 +7,41 @@ follow.
 """
 
 import requests
-import time
 import random
+from config import *
 from bs4 import BeautifulSoup
 
 BASE_URL = "https://en.wikipedia.org"
 
-def test_read():
-    curr_link = "https://en.wikipedia.org/wiki/43rd_Chess_Olympiad"
+def create_tweet(api):
+    """
+    This method pulls the most recent tweet from the account as the current
+    starting point to dive into. It picks a random link from this page, finds 
+    its title, then tweets that information.
+    """
+    # Get most recent link.
+    recent_tweet = api.user_timeline('WikiDiver')[0].text.split('-')
+    curr_link = recent_tweet[1][1:]
 
-    for i in range(15):
-        request = requests.get(curr_link)
-        soup = BeautifulSoup(request.text, 'html.parser')
-        links = soup.find_all('a')
-        poss_destinations = []
+    # Pull information from related page.
+    request = requests.get(curr_link)
+    soup = BeautifulSoup(request.text, 'html.parser')
+    links = soup.find_all('a')
+    poss_destinations = []
 
-        title = soup.find('h1', {"id": "firstHeading"}).text
-        print(title + " --- " + curr_link)
+    # Find all valid links to next pages.
+    for link in links:
+        if valid_link(link):
+            poss_destinations += [link]
 
-        for link in links:
-            if valid_link(link):
-                poss_destinations += [link]
+    # Get next link and find relating title.
+    curr_link = BASE_URL + random.choice(poss_destinations).get('href')
+    request = requests.get(curr_link)
+    soup = BeautifulSoup(request.text, 'html.parser')
+    title = soup.find('h1', {"id": "firstHeading"}).text
 
-        curr_link = BASE_URL + random.choice(poss_destinations).get('href')
+    # Return information to tweet.
+    return (title, curr_link)
 
 def valid_link(link):
     """
